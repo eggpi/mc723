bc87dce171ec0b432eb46042b6a08447e2c7a897
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2014-03-14 09:55:28 -0300

    use our mogrify.

--- benchmark.sh
+++ benchmark.sh
@@ -167,4 +167,4 @@ benchmark_gpg_encrypt prefix/bin/gpg data/home.tar
 benchmark_wav_to_mp3 prefix/bin/lame data/dracula_01_stoker.wav
 benchmark_bzip2_compress prefix/bin/bzip2 data/home.tar
 benchmark_ogv_to_mp4 prefix/bin/ffmpeg data/elephants_dream1.ogv
-benchmark_jpg_to_png mogrify "data/images/*.jpg"
+benchmark_jpg_to_png prefix/bin/mogrify "data/images/*.jpg"
